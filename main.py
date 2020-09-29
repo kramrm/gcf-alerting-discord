43706cf4ccce0ebc70c51bb008ba8da8c513770c
Kram <kramrm@gmail.com>
2020-09-29 15:22:03 -0700

    remove unused parse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import base64
 import json
-from dateutil.parser import parse
 from webhook import post_webhook
 from datetime import datetime
 
@@ -8,8 +7,8 @@ from datetime import datetime
 def hello_pubsub(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
+        event (dict): Event payload.
+        context (google.cloud.functions.Context): Metadata for the event.
     """
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     #post_webhook(message=f'{pubsub_message}', timestamp='now', status='status', title='title')
