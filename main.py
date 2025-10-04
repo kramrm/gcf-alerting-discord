beac4e408148f18c2385a25e4459f4a90b22fe48
kramrm <mark@mahacek.net>
2025-10-04 16:49:33 -0700

    consolidate scripts into single file

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+# gcf-alerting-discord.py
+
+"Google Cloud Function to send Google Cloud Monitoring alerts to Discord via webhook"
+
 import base64
 import json
-from webhook import post_webhook
-from datetime import datetime
+import os
+from datetime import datetime, timezone
+
+import requests
 
 
 def hello_pubsub(event, context):
@@ -10,24 +16,69 @@ def hello_pubsub(event, context):
         event (dict): Event payload.
         context (google.cloud.functions.Context): Metadata for the event.
     """
-    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    #post_webhook(message=f'{pubsub_message}', timestamp='now', status='status', title='title')
+    pubsub_message = base64.b64decode(event["data"]).decode("utf-8")
+    # post_webhook(message=f'{pubsub_message}', timestamp='now', status='status', title='title')
     message = json.loads(pubsub_message)
-    message = message['incident']
-    #post_webhook(message, timestamp, status, title='Monitoring'):
-    null = None
-    status = 'Status'
-    log_message = ''
-    title = 'Monitoring Alert'
-    status = message['state'].title()
-    timestamp = datetime.utcfromtimestamp(message["started_at"]).isoformat()
-    log_message += f'Started: {timestamp} UTC'
+    message = message["incident"]
+    # post_webhook(message, timestamp, status, title='Monitoring'):
+    status = "Status"
+    log_message = ""
+    title = "Monitoring Alert"
+    status = message["state"].title()
+    timestamp = datetime.fromtimestamp(
+        message["started_at"], tz=timezone.utc
+    ).isoformat()
+    log_message += f"Started: {timestamp} UTC"
     color = 16772608
-    if message['ended_at'] is not None:
-        timestamp = datetime.utcfromtimestamp(message["ended_at"]).isoformat()
-        log_message += f'\nEnded: {timestamp} UTC'
+    if message["ended_at"] is not None:
+        timestamp = datetime.fromtimestamp(
+            message["ended_at"], tz=timezone.utc
+        ).isoformat()
+        log_message += f"\nEnded: {timestamp} UTC"
         color = 65297
-    title = message['policy_name']
+    title = message["policy_name"]
     log_message += f'\n{message["summary"]}'
     log_message += f'\n[Monitor Event]({message["url"]})'
-    post_webhook(message=log_message, timestamp=timestamp, status=status, title=title, color=color)
+    post_webhook(
+        message=log_message,
+        timestamp=timestamp,
+        status=status,
+        title=title,
+        color=color,
+    )
+
+
+def post_webhook(message, timestamp, status, title, color=0):
+    """Post webhook to Discord
+    Set an environment variable for 'WEBHOOK' to point to the URI for your channel
+
+    Attributes
+        message (str): The message to put in the embed
+        timestamp (str): ISO 8601 timestamp of the event
+        title (str): Stats of the build process for the embed title.  Defaults to 'Status'
+        color (int): Color to use for embed highlight. Defaults to black
+    Returns
+        result (requests.result): Result of Requests post
+    """
+    url = os.environ.get("WEBHOOK")
+    if not url:
+        raise ValueError("No WEBHOOK env variable set")
+    data = {}
+    data["embeds"] = []
+    embed = {}
+    embed["title"] = f"{title} Notice"
+    embed["description"] = message
+    embed["footer"] = {}
+    embed["footer"]["text"] = f"Alert state: {status}"
+    embed["timestamp"] = timestamp
+    embed["color"] = color
+    data["embeds"].append(embed)
+    print(data)
+    result = requests.post(
+        url,
+        data=json.dumps(data),
+        headers={"Content-Type": "application/json"},
+        timeout=10,
+    )
+    print(result)
+    return result
